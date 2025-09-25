7471773105b12dacb10fcabb91ff46fa79999232
p1thong <phamnhatthong1712@gmail.com>
2025-09-25 18:32:27 +0700

    Thêm các folder

---
+++ ASM1.Service/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASM1.Service.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        
+    }
+}

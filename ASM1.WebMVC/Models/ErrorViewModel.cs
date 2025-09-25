218c4bde174e97e685777980a69557a9de5350ec
p1thong <phamnhatthong1712@gmail.com>
2025-09-25 18:24:08 +0700

    Initial

---
+++ ASM1.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASM1.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

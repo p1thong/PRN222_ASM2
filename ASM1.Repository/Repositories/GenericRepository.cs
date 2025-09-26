c52d21729b9a154d2086205b96f446f7afbb8d09
soloplayer345 <hongtlse172336@fpt.edu.vn>
2025-09-26 14:39:05 +0700

    Add generic repository interface and stub class
    Introduced IGenericRepository interface with common async CRUD operations and a placeholder GenericRepository class to support generic data access patterns.

---
+++ ASM1.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM1.Repository.Repositories
+{
+    internal class GenericRepository<T>
+    {
+    }
+}
---
+++ ASM1.Repository/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM1.Repository.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        void DeleteRange(IEnumerable<T> entities);
+
+        Task<int> SaveChangesAsync();
+    }
+}

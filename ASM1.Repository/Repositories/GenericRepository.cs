08cd649fb39a832fc6a431927b4573e7640a46b5
soloplayer345 <hongtlse172336@fpt.edu.vn>
2025-09-28 07:57:13 +0700

    Implement generic repository with CRUD operations
    Refactored GenericRepository to be public and implement IGenericRepository<T>. Added async CRUD methods for Add, AddRange, Delete, DeleteRange, Find, GetAll, GetById, SaveChanges, and Update using Entity Framework Core. This improves code reusability and standardizes data access patterns.

--- ASM1.Repository/Repositories/GenericRepository.cs
+++ ASM1.Repository/Repositories/GenericRepository.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ASM1.Repository.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace ASM1.Repository.Repositories
 {
-    internal class GenericRepository<T>
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await Task.Run(() =>
+            {
+                _dbSet.Update(entity);
+            });
+        }
     }
 }

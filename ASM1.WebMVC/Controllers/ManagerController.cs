90c3a92f3dddb5afc43854bb3ec353a576b9aa58
phantien1002 <phanquangtien1002@gmail.com>
2025-09-30 13:58:09 +0700

    dealer manager customers, test drive and feedback

---
+++ ASM1.WebMVC/Controllers/ManagerController.cs
@@ -0,0 +1,43 @@
+﻿using ASM1.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASM1.WebMVC.Controllers
+{
+    [Route("[controller]")]
+    public class ManagerController : Controller
+    {
+        private readonly IManagerService _managerService;
+        public ManagerController(IManagerService managerService)
+        {
+            _managerService = managerService;
+        }
+
+        [HttpGet("total-customer")]
+        public IActionResult GetTotalCustomer()
+        {
+            var total = _managerService.GetTotalCustomers();
+            return Ok(total);
+        }
+
+        [HttpGet("total-feedbacks")]
+        public IActionResult GetTotalFeedbacks()
+        {
+            var total = _managerService.GetTotalFeedbacks();
+            return Ok(total);
+        }
+
+        [HttpGet("total-testdrives")]
+        public IActionResult GetTotalTestDrives()
+        {
+            var total = _managerService.GetTotalTestDrives();
+            return Ok(total);
+        }
+
+        [HttpGet("customers")]
+        public IActionResult GetAllCustomers()
+        {
+            var customers = _managerService.GetAllCustomers();
+            return Ok(customers);
+        }
+    }
+}
